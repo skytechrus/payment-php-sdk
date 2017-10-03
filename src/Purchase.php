9baa2da309093c2d16cd49f69e0a03b5352149a5
arevkina <ea@skytecrussia.com>
2017-10-03 14:14:27 +0300

    init

---
+++ src/Purchase.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arevkina
+ * Date: 03.10.2017
+ * Time: 12:50
+ */
+
+namespace Skytech;
+
+
+class Purchase
+{    /**
+     * Purchase constructor.
+     */
+    public function __construct()
+    {
+    }
+}
+
