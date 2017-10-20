6054356457cabf94c83c8e5cdd88fd5f55b40e7c
Sergei Ivanov <msvbell@gmail.com>
2017-10-20 17:04:06 +0300

    refactor. move field and methods from Order

---
+++ src/Merchant.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msvbe
+ * Date: 20.10.2017
+ * Time: 16:23
+ */
+
+namespace Skytech;
+
+
+class Merchant
+{
+    private $language;
+    private $merchantId;
+
+    /**
+     * Merchant constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param mixed $language
+     */
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMerchantId()
+    {
+        return $this->merchantId;
+    }
+
+    /**
+     * @param mixed $merchantId
+     */
+    public function setMerchantId($merchantId)
+    {
+        $this->merchantId = $merchantId;
+    }
+
+}
