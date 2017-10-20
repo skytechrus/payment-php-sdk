d94b57406cdcc274ec54d4d7508df67407cd5cb5
Sergei Ivanov <msvbell@gmail.com>
2017-10-20 17:02:44 +0300

    init test

---
+++ tests/unit/OrderTest.php
@@ -0,0 +1,123 @@
+<?php
+
+
+class OrderTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @var \Skytech\Order
+     */
+    protected $order;
+
+    public function testSetAmount()
+    {
+        $amount = 123;
+        $this->order->setAmount($amount);
+        $this->assertEquals($amount, $this->order->getAmount(), 'Set amount as integer success');
+    }
+
+    public function testSetNumericAmountAsString()
+    {
+        $amount = '123';
+        $this->order->setAmount($amount);
+        $this->assertEquals($amount, $this->order->getAmount(), 'Set amount as string success');
+        $this->assertEquals((int)$amount, $this->order->getAmount(), 'Set amount as string success #2');
+    }
+
+    public function testSetAmountNotNumeric()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $amount = 'string';
+        $this->order->setAmount($amount);
+    }
+
+    public function testSetCurrency()
+    {
+        $currency = 643;
+        $this->order->setCurrency($currency);
+        $this->assertEquals($currency, $this->order->getCurrency());
+
+        $currency = '643';
+        $this->order->setCurrency($currency);
+        $this->assertEquals((int)$currency, $this->order->getCurrency());
+
+        $currency = 6433;
+        $this->expectException(InvalidArgumentException::class);
+        $this->order->setCurrency($currency);
+
+        $currency = 64;
+        $this->expectException(InvalidArgumentException::class);
+        $this->order->setCurrency($currency);
+
+        $currency = "string";
+        $this->expectException(InvalidArgumentException::class);
+        $this->order->setCurrency($currency);
+    }
+
+    public function testSetWrongCurrency()
+    {
+        $currency = "RUB";
+        $this->expectException(InvalidArgumentException::class);
+        $this->order->setCurrency($currency);
+    }
+
+    /**
+     * @depends testSetCurrency
+     */
+    public function testGetCurrency()
+    {
+        $currency = 643;
+        $this->order->setCurrency($currency);
+        $this->assertEquals($currency, $this->order->getCurrency());
+    }
+
+    public function testSetAndGetDesciption()
+    {
+        $description = "trololo";
+        $this->order->setDescription($description);
+        $this->assertEquals($description, $this->order->getDescription());
+    }
+
+    public function testSetAndGetOrderStatus()
+    {
+        $status = "success";
+        $this->order->setOrderStatus($status);
+        $this->assertEquals($status, $this->order->getOrderStatus());
+    }
+
+    public function testSetAndGetXId()
+    {
+        $xid = "654321";
+        $this->order->setXId($xid);
+        $this->assertEquals($xid, $this->order->getXId());
+    }
+
+    public function testSetAndGetOrderId()
+    {
+        $orderId = 234;
+        $this->order->setOrderId($orderId);
+        $this->assertEquals($orderId, $this->order->getOrderId());
+    }
+
+    public function testSetAndGetOrderDate()
+    {
+        $date = new DateTime();
+        $this->order->setOrderDate($date);
+        $this->assertInstanceOf(DateTime::class, $this->order->getOrderDate());
+    }
+
+
+    protected function _before()
+    {
+        $this->order = new \Skytech\Order();
+    }
+
+    protected function _after()
+    {
+    }
+
+}
