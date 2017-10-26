6228c9372203f3bd4b15d403bf538f19827461d0
arevkina <ea@skytecrussia.com>
2017-10-26 17:54:05 +0300

    Connector by GuzzleHttp fix DataProvider class

--- src/Connector.php
+++ src/Connector.php
@@ -12,6 +12,7 @@ use RuntimeException;
 use Skytech\Config\Config;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use Skytech\DataProvider\DataProvider;
 
 class Connector
 {
@@ -34,7 +35,14 @@ class Connector
 
         try {
             if (Config::getDataFormat() == Config::XMLDATA) {
-                $response = $client->request('POST', $url, ['body' => $this->orderdata,'allow_redirects' => false]);
+                $response = $client->request('POST', $url, ['body' => $this->orderdata,'allow_redirects' => [
+                    'max'             => 5,        // allow at most 5 redirects.
+                    'strict'          => true,      // use "strict" RFC compliant redirects.
+                    'referer'         => false,      // do not add a Referer header
+                    'protocols'       => ['https','http'], // only allow https URLs
+                    //'on_redirect'     => $onRedirect,
+                    'track_redirects' => true
+                ] ]);
                 $content = (string)$response->getBody();
             } elseif (Config::getDataFormat() == Config::JSON) {
                 $response = $client->request('POST', $url, ['json' => $this->orderdata,'allow_redirects' =>[
@@ -55,29 +63,6 @@ class Connector
             throw new RuntimeException('Request Exception:' . $e->getMessage());
         }
 
-
-        /*
-        $fp = fsockopen(Config::getHostName(), Config::getPort(), $errno, $errstr, 30);
-        $request_data = $this->orderdata->getRequestData(); //XML For request
-        if (!$fp) die('<p>'.$errstr.' ('.$errno.')</p>'); // Проверить установку соединения
-          // Заголовок HTTPS-запроса
-        $headers = 'POST '.$path." HTTP/1.0\r\n";
-        $headers .= 'Host: '.Config::getHostName()."\r\n";
-        $headers .= "Content-type: application/x-www-form-urlencoded\r\n";
-        $headers .= 'Content-Length: '.strlen($request_data)."\r\n\r\n";
-        // Отправить HTTPS-запрос серверу
-        fwrite($fp, $headers.$request_data);
-        // Получаем ответ
-        while ( !feof($fp) ){
-           $inStr= fgets($fp, 1024);
-           // В этом случае необходимо вырезать ответ
-           if (substr($inStr,0,7)!=="<TKKPG>") continue;
-           //Добавляем оставшися текст в строку ответа
-           $content .= $inStr;
-           }
-        // Закрыть соединение
-        fclose($fp);
-        */
         return ($content);
     }
 }
