9e33549efd23e5c82676cda9f21ab07c3a1a5454
Sergei Ivanov <msvbell@gmail.com>
2017-10-30 13:06:46 +0300

    refactor sendRequest

--- src/Connector.php
+++ src/Connector.php
@@ -37,32 +37,48 @@ class Connector
      */
     public function sendRequest()
     {
+        $url = $this->getUrl();
+        switch (Config::getDataFormat()) {
+            case Config::XML:
+                return $this->getResponse($url, $this->orderdata);
+            case Config::JSON:
+                return $this->getResponse($url, $this->orderdata);
+            default:
+                throw new \Exception('Invalid format');
+        }
+    }
+
+    /**
+     * @param $url
+     * @param $body
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     */
+    private function getResponse($url, $body)
+    {
         $client = new Client();
+        $response = $client->request('POST', $url, [
+            'body' => $body,
+            'allow_redirects' => [
+                'max' => 5,        // allow at most 5 redirects.
+                'strict' => true,      // use "strict" RFC compliant redirects.
+                'referer' => false,      // do not add a Referer header
+                'protocols' => ['https', 'http'], // only allow https URLs
+                //'on_redirect'     => $onRedirect,
+                'track_redirects' => true
+            ]
+        ]);
+        return new Response\ResponseStrategy($response);
+    }
+
+    /**
+     * @return string
+     */
+    private function getUrl()
+    {
         $url = Config::getHostName() . ':' . Config::getPort();
         if (!strpos($url, "://")) {
             $url = 'https://' . $url;
         }
-        switch (Config::getDataFormat()) {
-            case Config::XML:
-                $response = $client->request('POST', $url, ['body' => $this->orderdata, 'allow_redirects' => [
-                    'max' => 5,        // allow at most 5 redirects.
-                    'strict' => true,      // use "strict" RFC compliant redirects.
-                    'referer' => false,      // do not add a Referer header
-                    'protocols' => ['https', 'http'], // only allow https URLs
-                    //'on_redirect'     => $onRedirect,
-                    'track_redirects' => true]]);
-                return new Response\ResponseStrategy($response);
-            case Config::JSON:
-                $response = $client->request('POST', $url, ['json' => $this->orderdata, 'allow_redirects' => [
-                    'max' => 5,        // allow at most 5 redirects.
-                    'strict' => true,      // use "strict" RFC compliant redirects.
-                    'referer' => false,      // do not add a Referer header
-                    'protocols' => ['https', 'http'], // only allow https URLs
-                    //'on_redirect'     => $onRedirect,
-                    'track_redirects' => true]]);
-                return new Response\ResponseStrategy($response);
-            default:
-                throw new \Exception('Invalid format');
-        }
+        return $url;
     }
 }
