8d786f4a8fab46f9f82bf70abf530d79d6ba746d
Sergei Ivanov <msvbell@gmail.com>
2017-12-01 13:19:33 +0300

    temp server url

--- src/Config/config_ini.php
+++ src/Config/config_ini.php
@@ -8,6 +8,5 @@ return array(
     'testing' => true,
     'PROD' => array('hostname'=>'123.123.12.3',
                    'port'=>'1234'),
-    'TEST' => array('hostname'=>'456.456.45.6',
-                   'port'=>'567')
+    'TEST' => array('hostname'=>'https://mpi.skytecrussia.com')
 );
